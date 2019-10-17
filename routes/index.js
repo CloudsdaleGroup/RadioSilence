74e3ed5ae59664710ca681a033ee23701168e861
Asthriona <im@asthriona.com>
2019-10-17 22:16:43 +0200

    Build site base

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Radio Silence' });
+  res.render('index', { title: 'Radio Silence', layout: 'layfooter' });
 });
 router.get('/planning', function(req, res, next) {
-  res.render('planning', { title: 'Radio Silence - Planing' });
+  res.render('planning', { title: 'Radio Silence - Planing', layout: 'layfooter' });
 });
 router.get('/team', function(req, res, next) {
   res.render('team', { title: 'Radio Silence - Team' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Radio Silence - A Prpopos', layout: 'layfooter' });
 });
 
 module.exports = router;
